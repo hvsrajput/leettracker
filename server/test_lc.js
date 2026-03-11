888318758595cf2e628677901702ffb2f8959471
sahaymihir <sahaymihir@users.noreply.github.com>
2026-03-11 06:40:17 +0530

    chore: remove test script

--- server/test_lc.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-async function test() {
-  const q = `query { recentAcSubmissionList(username: "hvsrajput", limit: 100) { titleSlug } }`;
-  const res = await axios.post('https://leetcode.com/graphql', { query: q });
-  console.log(res.data.data.recentAcSubmissionList.length);
-}
-test();
